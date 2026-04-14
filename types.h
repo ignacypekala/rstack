60249011575591ae44e6efdbf7f7eab4a2f09cd3
Ignacy Pękała <ignacy@pekala.eu>
2026-04-14 22:40:37 +0200

    Revert "Put visited and state in a union"
    This reverts commit 077fbae9af80495c94830ebab0dee217a2a7b240.

--- types.h
+++ types.h
@@ -15,9 +15,7 @@
 
 // Represents the lifecycle state of a node during trial deletion. 
 // It is the equivalent of a color in the Bacon/Rajan trial deletion algorithm.
-// 
-// Setting the type to unsigned char keeps the enum size down at 1 byte.
-enum rstack_gc_state : unsigned char {
+enum rstack_gc_state {
     NORMAL,
     UNDER_TRIAL,
     RESCUED,
@@ -30,13 +28,9 @@ typedef struct {
     size_t               size;
     size_t               capacity;
     uint64_t             references;
+    bool                 visited;
 
-    // Flag 'visited' is for standard traversal, enum 'state'
-    // is for garbage collection. They are never used together.
-    union {
-        bool                 visited;
-        enum rstack_gc_state state;
-    };
+    enum rstack_gc_state state;
 
     // Intrusive list pointer used to queue unreachable nodes during garbage
     // collection
