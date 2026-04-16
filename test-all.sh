907a1436033a00f6d9553bc83c5dcf949323ec7a
Ignacy Pękała <ignacy@pekala.eu>
2026-04-16 13:48:46 +0200

    Export tests to a submodule

--- test-all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for batch in ./tests_*/; do
-    batch_name=${batch#./tests_}
-    batch_name=${batch_name%/}
-    ./test-batch.sh $batch_name
-    echo
-done
--- tests_6_michal_bak/converter.sh
+++
@@ -1,8 +0,0 @@
-for file in ./output/test*_*.model.out; do
-    filename=${file#./output/}
-    testname=${filename%_*.model.out}
-    casename=${filename#${testname}_}
-    casename=${casename%.model.out}
-    mkdir $testname
-    mv "$file" "./$testname/${testname}_${casename}.fout"
-done
