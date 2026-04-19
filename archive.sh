26581b40a0de16a9b3f4da7b239219dd1ebbc956
Ignacy Pękała <ignacy@pekala.eu>
2026-04-19 20:42:27 +0200

    Handle Makefile-release

--- archive.sh
+++ archive.sh
@@ -1,5 +1,6 @@
 mkdir rstack
-cp rstack.c Makefile rstack_container.c rstack_container.h \
+cp Makefile-release rstack/
+cp rstack.c rstack_container.c rstack_container.h \
     rstack_delete.c rstack_delete.h  rstack_read.c types.h \
     rstack/
 
