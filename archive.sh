344755cbc39aa23d4091ff7a7b4b8ca9e476a705
Ignacy Pękała <ignacy@pekala.eu>
2026-04-20 18:09:59 +0200

    Remove subfolder from archive

--- archive.sh
+++ archive.sh
@@ -1,7 +1,10 @@
 mkdir rstack
-cp Makefile-release rstack/
-cp rstack.c rstack_container.c rstack_container.h \
+cp Makefile-release rstack/Makefile
+cp rstack.c rstack.h rstack_container.c rstack_container.h \
     rstack_delete.c rstack_delete.h  rstack_read.c types.h \
     rstack/
 
-tar -czvf 'rstack.tgz' rstack
+cd rstack
+tar -czvf 'rstack.tgz' *
+mv rstack.tgz ..
+cd -
